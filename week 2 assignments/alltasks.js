14928a3f7a085b6787170cab7e93c44c2a4a9fcf
akash-arelli <akasharelli@gmail.com>
2020-07-04 12:17:46 +0530

    task 1

---
+++ week 2 assignments/alltasks.js
@@ -0,0 +1,28 @@
+   function dis(val) 
+         { 
+             return document.getElementById("result").value+=val; 
+         } 
+           
+          
+         function solve() 
+         { 
+             
+             let x = eval(document.getElementById("result").value ) 
+            
+             return x;
+         } 
+           
+         
+         function clr() 
+         { 
+             return ""; 
+         } 
+         function abs(val) {
+  
+return Math.abs(val);
+}
+
+  function domath(val) {
+  
+return Math.sqrt(val);
+}
